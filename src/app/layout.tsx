04ff98bc8db51588a51d54d3e384b9320e8edbe1
sutanth19 <sutanthkunsuraman@gmail.com>
2025-07-21 15:36:10 +0800

    fix: successful build without Google Fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,24 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import MainNavbar from "@/components/MainNavbar";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Sutanth | Portfolio",
   description: "My personal portfolio website",
   icons: {
-    icon: "/favicon-32x32.png", 
+    icon: "/favicon-32x32.png",
   },
 };
 
@@ -29,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+      <body className="antialiased">
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <MainNavbar />
           {children}
